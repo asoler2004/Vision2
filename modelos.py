a04b86438e58671e322752b4d2b27443188ffd64
antonia <asoler2004@gmail.com>
2025-11-29 14:27:11 -0300

    deleted hardcoded keys

--- modelos.py
+++ modelos.py
@@ -8,8 +8,7 @@ import torch
 # Cargar variables de entorno
 load_dotenv()
 
-api_key = "AIzaSyBu2PEE7XZoFD-fpyzkOA6NsO0JXSxGOXk"
-print(os.getenv("GEMINI_API_KEY"))
+api_key = os.getenv("GEMINI_API_KEY")
 if not api_key:
     raise RuntimeError("Falta GEMINI_API_KEY en .env")
 
